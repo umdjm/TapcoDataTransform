bb7733d7415c38abbcc9d348ef2273d12d349dac
DJ <umdjm@hotmail.com>
2013-11-13 20:39:21 -0500

    Defaulting required to true

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,10 @@
 class Question < ActiveRecord::Base
   attr_accessible :required, :answerOptions, :answerType, :questionText, :question_set_id, :fieldname, :image_path, :orderingcolumn
   belongs_to "QuestionSet", class_name: "QuestionSet", foreign_key: :question_set_id
+
+  after_initialize do
+    if self.new_record?
+      self.required = true
+    end
+  end
 end
